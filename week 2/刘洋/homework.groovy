1922b0256675911ba54a2f9c278cd9216725548d
web <liuyangc33@gmail.com>
2016-04-14 22:56:21 +0800

    Update homework.groovy

--- week 2/刘洋/homework.groovy
+++ week 2/刘洋/homework.groovy
@@ -12,3 +12,23 @@ def insertionSort(array) {
 }
 
 println insertionSort([7, 0, 3, 6, 4])
+
+def ShellSort(array) {
+    def length = array.size()
+    def h = length.intdiv(2)
+    while (h > 0) {
+        for (i = h; i < length; i++) {
+            for (j = i; j > 0; j = j - h) {
+                if (array[j - h] > array[j]) {
+                    swap(array, j - h, j)
+                } else {
+                    break
+                }
+            }
+        }
+        h = h.intdiv(2)
+    }
+    return array
+}
+
+println(ShellSort([4, 6, 2, 8, 0, 7, -1]))
