58c3427d845c88e9beb718adb178b5ce9cee97a3
liuyangc3 <liuyang@nxin.com>
2018-11-21 18:29:11 +0800

    update script

--- add_new_dir.py
+++ add_new_dir.py
@@ -1,14 +1,10 @@
 # -*- coding:utf-8 -*-
 
-import os
+import re
+from pathlib import Path
 
-_max = 0
-for d in os.listdir(os.getcwd()):
-    if d.startswith("week"):
-        index = int(d[5:])
-        _max = max(index, _max)
-dir_name = "week-{0}".format(_max+1)
-os.mkdir(dir_name)
-with open("{0}/readme.md".format(dir_name), 'w') as f:
-    f.close()
-
+pwd = Path('.')
+new = 'week-{}'.format(
+    max([int(re.split('[ -]', d.name)[1]) for d in Path('.').iterdir() if d.name.startswith("week")]) + 1
+)
+(pwd / new).mkdir()
